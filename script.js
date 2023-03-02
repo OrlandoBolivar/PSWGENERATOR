c677c2b30bb1daf129cd645f38d25f6b93fe54c3
OrlandoBolivar <123449213+OrlandoBolivar@users.noreply.github.com>
2023-03-02 22:47:49 +1100

    more code

--- script.js
+++ script.js
@@ -34,12 +34,19 @@ function generatePassword() {
 
 // TODO: See if ateleast one  char type is chosen, if not , return with an alert saing atleast one char type should be selected for 
 // password to be generated
-  
+
 // You need to generated one random characcter out of the all the valid characers inn a loop, add that character to genPsw and keep 
 //repeating it for pswLength times
 
+let randomIndex = 0;
+let randomChar = "";
+for (let i=0; i <pswLength ; i++){
+  randomIndex = Math.floor ( Math.random() * validChars.length);
+  randomChar = validChars[randomIndex];
+  genPsw += randomChar;
+}
 //Finallly return the genPsw which holds the generatedpassword
-
+return genPsw;
 
 }
 
